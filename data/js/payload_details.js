a9ef201201701f010fc3c2b88264637330cc60a8
Darren Kuropatwa <dkuropatwa@gmail.com>
2017-07-11 23:16:15 -0500

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 24637,
-	"created_at": "Tue Jul 04 21:16:14 +0000 2017",
+	"tweets": 24638,
+	"created_at": "Wed Jul 12 04:16:14 +0000 2017",
 	"lang": "en"
 }
--- data/js/tweets/2017_07.js
+++ data/js/tweets/2017_07.js
@@ -1,6 +1,219 @@
 Grailbird.data.tweets_2017_07 = 
 [
 	{
+		"created_at": "Wed Jul 12 03:40:13 +0000 2017",
+		"id": 884980691313864700,
+		"id_str": "884980691313864704",
+		"text": "RT @SkipPrichard: “There is nobility in compassion, a beauty in empathy, a grace in forgiveness.” -John Connolly https://t.co/kSpdJUNfjf #q…",
+		"truncated": false,
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "SkipPrichard",
+					"name": "Skip Prichard",
+					"id": 361468994,
+					"id_str": "361468994",
+					"indices": [
+						3,
+						16
+					]
+				}
+			],
+			"urls": [
+				{
+					"url": "https://t.co/kSpdJUNfjf",
+					"expanded_url": "http://buff.ly/2u95d01",
+					"display_url": "buff.ly/2u95d01",
+					"indices": [
+						113,
+						136
+					]
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 5634392,
+			"id_str": "5634392",
+			"name": "Darren Kuropatwa",
+			"screen_name": "dkuropatwa",
+			"location": "Winnipeg, Manitoba, Canada",
+			"description": "Director of Learning at Hanover School Division \nAlways in beta ∞β \n(Tweets represent my own views.)",
+			"url": "http://t.co/FJEo2rqZf5",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "http://t.co/FJEo2rqZf5",
+							"expanded_url": "http://adifference.blogspot.com",
+							"display_url": "adifference.blogspot.com",
+							"indices": [
+								0,
+								22
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 6912,
+			"friends_count": 2588,
+			"listed_count": 468,
+			"created_at": "Sun Apr 29 16:11:17 +0000 2007",
+			"favourites_count": 378,
+			"utc_offset": -18000,
+			"time_zone": "Central Time (US & Canada)",
+			"geo_enabled": false,
+			"verified": true,
+			"statuses_count": 24632,
+			"lang": "en",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "9AE4E8",
+			"profile_background_image_url": "http://pbs.twimg.com/profile_background_images/623182/ADifferenceBG.jpg",
+			"profile_background_image_url_https": "https://pbs.twimg.com/profile_background_images/623182/ADifferenceBG.jpg",
+			"profile_background_tile": true,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1274402174/dkuropatwaGuitar_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1274402174/dkuropatwaGuitar_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/5634392/1399343584",
+			"profile_link_color": "0000FF",
+			"profile_sidebar_border_color": "FFFFFF",
+			"profile_sidebar_fill_color": "67CD67",
+			"profile_text_color": "000000",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"retweeted_status": {
+			"created_at": "Wed Jul 12 03:37:03 +0000 2017",
+			"id": 884979893758550000,
+			"id_str": "884979893758550017",
+			"text": "“There is nobility in compassion, a beauty in empathy, a grace in forgiveness.” -John Connolly… https://t.co/v0Mk6EWvno",
+			"truncated": true,
+			"entities": {
+				"hashtags": [],
+				"symbols": [],
+				"user_mentions": [],
+				"urls": [
+					{
+						"url": "https://t.co/v0Mk6EWvno",
+						"expanded_url": "https://twitter.com/i/web/status/884979893758550017",
+						"display_url": "twitter.com/i/web/status/8…",
+						"indices": [
+							96,
+							119
+						]
+					}
+				]
+			},
+			"source": "<a href=\"http://bufferapp.com\" rel=\"nofollow\">Buffer</a>",
+			"in_reply_to_status_id": null,
+			"in_reply_to_status_id_str": null,
+			"in_reply_to_user_id": null,
+			"in_reply_to_user_id_str": null,
+			"in_reply_to_screen_name": null,
+			"user": {
+				"id": 361468994,
+				"id_str": "361468994",
+				"name": "Skip Prichard",
+				"screen_name": "SkipPrichard",
+				"location": "Columbus, OH",
+				"description": "CEO on a mission to share ideas & inspiration. Tips from bestselling #authors. (No DM's). Tweets don't = agreement. *FREE leadership ebook on my site.*",
+				"url": "https://t.co/r2vDLsoYU5",
+				"entities": {
+					"url": {
+						"urls": [
+							{
+								"url": "https://t.co/r2vDLsoYU5",
+								"expanded_url": "http://www.skipprichard.com",
+								"display_url": "skipprichard.com",
+								"indices": [
+									0,
+									23
+								]
+							}
+						]
+					},
+					"description": {
+						"urls": []
+					}
+				},
+				"protected": false,
+				"followers_count": 462139,
+				"friends_count": 378559,
+				"listed_count": 3988,
+				"created_at": "Wed Aug 24 20:47:28 +0000 2011",
+				"favourites_count": 1078,
+				"utc_offset": -14400,
+				"time_zone": "Eastern Time (US & Canada)",
+				"geo_enabled": false,
+				"verified": true,
+				"statuses_count": 26180,
+				"lang": "en",
+				"contributors_enabled": false,
+				"is_translator": false,
+				"is_translation_enabled": false,
+				"profile_background_color": "222222",
+				"profile_background_image_url": "http://pbs.twimg.com/profile_background_images/636919705/fbu5ao6hjmwv1y5pue32.jpeg",
+				"profile_background_image_url_https": "https://pbs.twimg.com/profile_background_images/636919705/fbu5ao6hjmwv1y5pue32.jpeg",
+				"profile_background_tile": false,
+				"profile_image_url": "http://pbs.twimg.com/profile_images/559119274434777088/ONIb0ImG_normal.jpeg",
+				"profile_image_url_https": "https://pbs.twimg.com/profile_images/559119274434777088/ONIb0ImG_normal.jpeg",
+				"profile_banner_url": "https://pbs.twimg.com/profile_banners/361468994/1353181044",
+				"profile_link_color": "222222",
+				"profile_sidebar_border_color": "520708",
+				"profile_sidebar_fill_color": "AFB2B3",
+				"profile_text_color": "333333",
+				"profile_use_background_image": true,
+				"has_extended_profile": true,
+				"default_profile": false,
+				"default_profile_image": false,
+				"following": true,
+				"follow_request_sent": false,
+				"notifications": true,
+				"translator_type": "none"
+			},
+			"geo": null,
+			"coordinates": null,
+			"place": null,
+			"contributors": null,
+			"is_quote_status": false,
+			"retweet_count": 2,
+			"favorite_count": 3,
+			"favorited": false,
+			"retweeted": true,
+			"possibly_sensitive": false,
+			"lang": "en"
+		},
+		"is_quote_status": false,
+		"retweet_count": 2,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en"
+	},
+	{
 		"created_at": "Tue Jul 04 21:05:43 +0000 2017",
 		"id": 882344696382709800,
 		"id_str": "882344696382709765",
