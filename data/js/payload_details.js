eeb68507e24f252b648918b0de7728768f4c80be
Darren Kuropatwa <dkuropatwa@gmail.com>
2017-08-15 17:16:16 -0500

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 24733,
-	"created_at": "Mon Aug 14 21:16:14 +0000 2017",
+	"tweets": 24734,
+	"created_at": "Tue Aug 15 22:16:15 +0000 2017",
 	"lang": "en"
 }
--- data/js/tweets/2017_08.js
+++ data/js/tweets/2017_08.js
@@ -1,6 +1,400 @@
 Grailbird.data.tweets_2017_08 = 
 [
 	{
+		"created_at": "Tue Aug 15 22:15:33 +0000 2017",
+		"id": 897582559936340000,
+		"id_str": "897582559936339968",
+		"text": "RT @shedevil1973: #brainfriendlylearning @tcaseylp @RobynPoulsen @kassmariano @LindsayTheresa @dkuropatwa @HaileyBitner https://t.co/YULEYT…",
+		"truncated": false,
+		"entities": {
+			"hashtags": [
+				{
+					"text": "brainfriendlylearning",
+					"indices": [
+						18,
+						40
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "shedevil1973",
+					"name": "Kboz",
+					"id": 1562118504,
+					"id_str": "1562118504",
+					"indices": [
+						3,
+						16
+					]
+				},
+				{
+					"screen_name": "tcaseylp",
+					"name": "Tammy Casey",
+					"id": 2192222956,
+					"id_str": "2192222956",
+					"indices": [
+						41,
+						50
+					]
+				},
+				{
+					"screen_name": "RobynPoulsen",
+					"name": "Robyn Poulsen",
+					"id": 1249147266,
+					"id_str": "1249147266",
+					"indices": [
+						51,
+						64
+					]
+				},
+				{
+					"screen_name": "kassmariano",
+					"name": "Kassandra Mariano",
+					"id": 1618100377,
+					"id_str": "1618100377",
+					"indices": [
+						65,
+						77
+					]
+				},
+				{
+					"screen_name": "LindsayTheresa",
+					"name": "Theresa Lindsay",
+					"id": 417323828,
+					"id_str": "417323828",
+					"indices": [
+						78,
+						93
+					]
+				},
+				{
+					"screen_name": "dkuropatwa",
+					"name": "Darren Kuropatwa",
+					"id": 5634392,
+					"id_str": "5634392",
+					"indices": [
+						94,
+						105
+					]
+				},
+				{
+					"screen_name": "HaileyBitner",
+					"name": "Mrs. Bitner",
+					"id": 2412553135,
+					"id_str": "2412553135",
+					"indices": [
+						106,
+						119
+					]
+				}
+			],
+			"urls": []
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 5634392,
+			"id_str": "5634392",
+			"name": "Darren Kuropatwa",
+			"screen_name": "dkuropatwa",
+			"location": "Winnipeg, Manitoba, Canada",
+			"description": "Director of Learning at Hanover School Division \nAlways in beta ∞β \n(Tweets represent my own views.)",
+			"url": "http://t.co/FJEo2rqZf5",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "http://t.co/FJEo2rqZf5",
+							"expanded_url": "http://adifference.blogspot.com",
+							"display_url": "adifference.blogspot.com",
+							"indices": [
+								0,
+								22
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 6945,
+			"friends_count": 2595,
+			"listed_count": 467,
+			"created_at": "Sun Apr 29 16:11:17 +0000 2007",
+			"favourites_count": 381,
+			"utc_offset": -18000,
+			"time_zone": "Central Time (US & Canada)",
+			"geo_enabled": false,
+			"verified": true,
+			"statuses_count": 24728,
+			"lang": "en",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "9AE4E8",
+			"profile_background_image_url": "http://pbs.twimg.com/profile_background_images/623182/ADifferenceBG.jpg",
+			"profile_background_image_url_https": "https://pbs.twimg.com/profile_background_images/623182/ADifferenceBG.jpg",
+			"profile_background_tile": true,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1274402174/dkuropatwaGuitar_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1274402174/dkuropatwaGuitar_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/5634392/1399343584",
+			"profile_link_color": "0000FF",
+			"profile_sidebar_border_color": "FFFFFF",
+			"profile_sidebar_fill_color": "67CD67",
+			"profile_text_color": "000000",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"retweeted_status": {
+			"created_at": "Tue Aug 15 21:44:50 +0000 2017",
+			"id": 897574831755071500,
+			"id_str": "897574831755071490",
+			"text": "#brainfriendlylearning @tcaseylp @RobynPoulsen @kassmariano @LindsayTheresa @dkuropatwa @HaileyBitner https://t.co/YULEYTpKcm",
+			"truncated": false,
+			"entities": {
+				"hashtags": [
+					{
+						"text": "brainfriendlylearning",
+						"indices": [
+							0,
+							22
+						]
+					}
+				],
+				"symbols": [],
+				"user_mentions": [
+					{
+						"screen_name": "tcaseylp",
+						"name": "Tammy Casey",
+						"id": 2192222956,
+						"id_str": "2192222956",
+						"indices": [
+							23,
+							32
+						]
+					},
+					{
+						"screen_name": "RobynPoulsen",
+						"name": "Robyn Poulsen",
+						"id": 1249147266,
+						"id_str": "1249147266",
+						"indices": [
+							33,
+							46
+						]
+					},
+					{
+						"screen_name": "kassmariano",
+						"name": "Kassandra Mariano",
+						"id": 1618100377,
+						"id_str": "1618100377",
+						"indices": [
+							47,
+							59
+						]
+					},
+					{
+						"screen_name": "LindsayTheresa",
+						"name": "Theresa Lindsay",
+						"id": 417323828,
+						"id_str": "417323828",
+						"indices": [
+							60,
+							75
+						]
+					},
+					{
+						"screen_name": "dkuropatwa",
+						"name": "Darren Kuropatwa",
+						"id": 5634392,
+						"id_str": "5634392",
+						"indices": [
+							76,
+							87
+						]
+					},
+					{
+						"screen_name": "HaileyBitner",
+						"name": "Mrs. Bitner",
+						"id": 2412553135,
+						"id_str": "2412553135",
+						"indices": [
+							88,
+							101
+						]
+					}
+				],
+				"urls": [],
+				"media": [
+					{
+						"id": 897574823278366700,
+						"id_str": "897574823278366721",
+						"indices": [
+							102,
+							125
+						],
+						"media_url": "http://pbs.twimg.com/media/DHTTi9TXUAEMLMU.jpg",
+						"media_url_https": "https://pbs.twimg.com/media/DHTTi9TXUAEMLMU.jpg",
+						"url": "https://t.co/YULEYTpKcm",
+						"display_url": "pic.twitter.com/YULEYTpKcm",
+						"expanded_url": "https://twitter.com/shedevil1973/status/897574831755071490/photo/1",
+						"type": "photo",
+						"sizes": {
+							"large": {
+								"w": 2048,
+								"h": 1536,
+								"resize": "fit"
+							},
+							"small": {
+								"w": 680,
+								"h": 510,
+								"resize": "fit"
+							},
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							},
+							"medium": {
+								"w": 1200,
+								"h": 900,
+								"resize": "fit"
+							}
+						}
+					}
+				]
+			},
+			"extended_entities": {
+				"media": [
+					{
+						"id": 897574823278366700,
+						"id_str": "897574823278366721",
+						"indices": [
+							102,
+							125
+						],
+						"media_url": "http://pbs.twimg.com/media/DHTTi9TXUAEMLMU.jpg",
+						"media_url_https": "https://pbs.twimg.com/media/DHTTi9TXUAEMLMU.jpg",
+						"url": "https://t.co/YULEYTpKcm",
+						"display_url": "pic.twitter.com/YULEYTpKcm",
+						"expanded_url": "https://twitter.com/shedevil1973/status/897574831755071490/photo/1",
+						"type": "photo",
+						"sizes": {
+							"large": {
+								"w": 2048,
+								"h": 1536,
+								"resize": "fit"
+							},
+							"small": {
+								"w": 680,
+								"h": 510,
+								"resize": "fit"
+							},
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							},
+							"medium": {
+								"w": 1200,
+								"h": 900,
+								"resize": "fit"
+							}
+						}
+					}
+				]
+			},
+			"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+			"in_reply_to_status_id": null,
+			"in_reply_to_status_id_str": null,
+			"in_reply_to_user_id": null,
+			"in_reply_to_user_id_str": null,
+			"in_reply_to_screen_name": null,
+			"user": {
+				"id": 1562118504,
+				"id_str": "1562118504",
+				"name": "Kboz",
+				"screen_name": "shedevil1973",
+				"location": "",
+				"description": "",
+				"url": null,
+				"entities": {
+					"description": {
+						"urls": []
+					}
+				},
+				"protected": false,
+				"followers_count": 36,
+				"friends_count": 209,
+				"listed_count": 2,
+				"created_at": "Tue Jul 02 03:50:22 +0000 2013",
+				"favourites_count": 285,
+				"utc_offset": null,
+				"time_zone": null,
+				"geo_enabled": false,
+				"verified": false,
+				"statuses_count": 146,
+				"lang": "en",
+				"contributors_enabled": false,
+				"is_translator": false,
+				"is_translation_enabled": false,
+				"profile_background_color": "C0DEED",
+				"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+				"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+				"profile_background_tile": false,
+				"profile_image_url": "http://pbs.twimg.com/profile_images/880872462694703104/2jd-t4Y6_normal.jpg",
+				"profile_image_url_https": "https://pbs.twimg.com/profile_images/880872462694703104/2jd-t4Y6_normal.jpg",
+				"profile_banner_url": "https://pbs.twimg.com/profile_banners/1562118504/1500575068",
+				"profile_link_color": "1DA1F2",
+				"profile_sidebar_border_color": "C0DEED",
+				"profile_sidebar_fill_color": "DDEEF6",
+				"profile_text_color": "333333",
+				"profile_use_background_image": true,
+				"has_extended_profile": false,
+				"default_profile": true,
+				"default_profile_image": false,
+				"following": false,
+				"follow_request_sent": false,
+				"notifications": false,
+				"translator_type": "none"
+			},
+			"geo": null,
+			"coordinates": null,
+			"place": null,
+			"contributors": null,
+			"is_quote_status": false,
+			"retweet_count": 1,
+			"favorite_count": 2,
+			"favorited": false,
+			"retweeted": true,
+			"possibly_sensitive": false,
+			"lang": "und"
+		},
+		"is_quote_status": false,
+		"retweet_count": 1,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": true,
+		"lang": "und"
+	},
+	{
 		"created_at": "Mon Aug 14 20:44:10 +0000 2017",
 		"id": 897197177847656400,
 		"id_str": "897197177847656448",
