b259143dc9a34ae5f00fd144bda62a1cfedd18b1
Darren Kuropatwa <dkuropatwa@gmail.com>
2017-12-12 23:16:15 -0600

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 24742,
-	"created_at": "Wed Dec 13 02:16:14 +0000 2017",
+	"tweets": 24743,
+	"created_at": "Wed Dec 13 05:16:14 +0000 2017",
 	"lang": "en"
 }
--- data/js/tweets/2017_12.js
+++ data/js/tweets/2017_12.js
@@ -1,6 +1,373 @@
 Grailbird.data.tweets_2017_12 = 
 [
 	{
+		"created_at": "Wed Dec 13 04:30:49 +0000 2017",
+		"id": 940801156791636000,
+		"id_str": "940801156791635970",
+		"text": "RT @sewilkie: Happy, Happy Hannukah to our friends and family beginning their feast of lights tonight! https://t.co/G39CoEKfGd",
+		"truncated": false,
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "sewilkie",
+					"name": "sara wilkie",
+					"id": 244625496,
+					"id_str": "244625496",
+					"indices": [
+						3,
+						12
+					]
+				}
+			],
+			"urls": [],
+			"media": [
+				{
+					"id": 940793048015036400,
+					"id_str": "940793048015036416",
+					"indices": [
+						103,
+						126
+					],
+					"media_url": "http://pbs.twimg.com/media/DQ5eSuoX4AA0NFU.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/DQ5eSuoX4AA0NFU.jpg",
+					"url": "https://t.co/G39CoEKfGd",
+					"display_url": "pic.twitter.com/G39CoEKfGd",
+					"expanded_url": "https://twitter.com/sewilkie/status/940793053656305664/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 534,
+							"h": 401,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 534,
+							"h": 401,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 534,
+							"h": 401,
+							"resize": "fit"
+						}
+					},
+					"source_status_id": 940793053656305700,
+					"source_status_id_str": "940793053656305664",
+					"source_user_id": 244625496,
+					"source_user_id_str": "244625496"
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 940793048015036400,
+					"id_str": "940793048015036416",
+					"indices": [
+						103,
+						126
+					],
+					"media_url": "http://pbs.twimg.com/media/DQ5eSuoX4AA0NFU.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/DQ5eSuoX4AA0NFU.jpg",
+					"url": "https://t.co/G39CoEKfGd",
+					"display_url": "pic.twitter.com/G39CoEKfGd",
+					"expanded_url": "https://twitter.com/sewilkie/status/940793053656305664/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 534,
+							"h": 401,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 534,
+							"h": 401,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 534,
+							"h": 401,
+							"resize": "fit"
+						}
+					},
+					"source_status_id": 940793053656305700,
+					"source_status_id_str": "940793053656305664",
+					"source_user_id": 244625496,
+					"source_user_id_str": "244625496"
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/#!/download/ipad\" rel=\"nofollow\">Twitter for iPad</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 5634392,
+			"id_str": "5634392",
+			"name": "Darren Kuropatwa",
+			"screen_name": "dkuropatwa",
+			"location": "Winnipeg, Manitoba, Canada",
+			"description": "Director of Learning at Hanover School Division \nAlways in beta ∞β \n(Tweets represent my own views.)",
+			"url": "http://t.co/FJEo2rqZf5",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "http://t.co/FJEo2rqZf5",
+							"expanded_url": "http://adifference.blogspot.com",
+							"display_url": "adifference.blogspot.com",
+							"indices": [
+								0,
+								22
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 6939,
+			"friends_count": 2604,
+			"listed_count": 465,
+			"created_at": "Sun Apr 29 16:11:17 +0000 2007",
+			"favourites_count": 385,
+			"utc_offset": -21600,
+			"time_zone": "Central Time (US & Canada)",
+			"geo_enabled": false,
+			"verified": true,
+			"statuses_count": 24733,
+			"lang": "en",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "9AE4E8",
+			"profile_background_image_url": "http://pbs.twimg.com/profile_background_images/623182/ADifferenceBG.jpg",
+			"profile_background_image_url_https": "https://pbs.twimg.com/profile_background_images/623182/ADifferenceBG.jpg",
+			"profile_background_tile": true,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1274402174/dkuropatwaGuitar_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1274402174/dkuropatwaGuitar_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/5634392/1399343584",
+			"profile_link_color": "0000FF",
+			"profile_sidebar_border_color": "FFFFFF",
+			"profile_sidebar_fill_color": "67CD67",
+			"profile_text_color": "000000",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"retweeted_status": {
+			"created_at": "Wed Dec 13 03:58:37 +0000 2017",
+			"id": 940793053656305700,
+			"id_str": "940793053656305664",
+			"text": "Happy, Happy Hannukah to our friends and family beginning their feast of lights tonight! https://t.co/G39CoEKfGd",
+			"truncated": false,
+			"entities": {
+				"hashtags": [],
+				"symbols": [],
+				"user_mentions": [],
+				"urls": [],
+				"media": [
+					{
+						"id": 940793048015036400,
+						"id_str": "940793048015036416",
+						"indices": [
+							89,
+							112
+						],
+						"media_url": "http://pbs.twimg.com/media/DQ5eSuoX4AA0NFU.jpg",
+						"media_url_https": "https://pbs.twimg.com/media/DQ5eSuoX4AA0NFU.jpg",
+						"url": "https://t.co/G39CoEKfGd",
+						"display_url": "pic.twitter.com/G39CoEKfGd",
+						"expanded_url": "https://twitter.com/sewilkie/status/940793053656305664/photo/1",
+						"type": "photo",
+						"sizes": {
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							},
+							"medium": {
+								"w": 534,
+								"h": 401,
+								"resize": "fit"
+							},
+							"large": {
+								"w": 534,
+								"h": 401,
+								"resize": "fit"
+							},
+							"small": {
+								"w": 534,
+								"h": 401,
+								"resize": "fit"
+							}
+						}
+					}
+				]
+			},
+			"extended_entities": {
+				"media": [
+					{
+						"id": 940793048015036400,
+						"id_str": "940793048015036416",
+						"indices": [
+							89,
+							112
+						],
+						"media_url": "http://pbs.twimg.com/media/DQ5eSuoX4AA0NFU.jpg",
+						"media_url_https": "https://pbs.twimg.com/media/DQ5eSuoX4AA0NFU.jpg",
+						"url": "https://t.co/G39CoEKfGd",
+						"display_url": "pic.twitter.com/G39CoEKfGd",
+						"expanded_url": "https://twitter.com/sewilkie/status/940793053656305664/photo/1",
+						"type": "photo",
+						"sizes": {
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							},
+							"medium": {
+								"w": 534,
+								"h": 401,
+								"resize": "fit"
+							},
+							"large": {
+								"w": 534,
+								"h": 401,
+								"resize": "fit"
+							},
+							"small": {
+								"w": 534,
+								"h": 401,
+								"resize": "fit"
+							}
+						}
+					}
+				]
+			},
+			"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+			"in_reply_to_status_id": null,
+			"in_reply_to_status_id_str": null,
+			"in_reply_to_user_id": null,
+			"in_reply_to_user_id_str": null,
+			"in_reply_to_screen_name": null,
+			"user": {
+				"id": 244625496,
+				"id_str": "244625496",
+				"name": "sara wilkie",
+				"screen_name": "sewilkie",
+				"location": "Beverly Hills, MI ",
+				"description": "Head of Learning Design, November Learning Learner, Teacher, Disruptor...of a most amazing learning team https://t.co/lOU096r3mQ header via slackpics",
+				"url": "https://t.co/27T1QlEtPX",
+				"entities": {
+					"url": {
+						"urls": [
+							{
+								"url": "https://t.co/27T1QlEtPX",
+								"expanded_url": "http://balancedtech.wikispaces.com",
+								"display_url": "balancedtech.wikispaces.com",
+								"indices": [
+									0,
+									23
+								]
+							}
+						]
+					},
+					"description": {
+						"urls": [
+							{
+								"url": "https://t.co/lOU096r3mQ",
+								"expanded_url": "http://BalancEdTech.wikispaces.com",
+								"display_url": "BalancEdTech.wikispaces.com",
+								"indices": [
+									105,
+									128
+								]
+							}
+						]
+					}
+				},
+				"protected": false,
+				"followers_count": 2301,
+				"friends_count": 758,
+				"listed_count": 136,
+				"created_at": "Sat Jan 29 19:04:54 +0000 2011",
+				"favourites_count": 1916,
+				"utc_offset": -18000,
+				"time_zone": "Quito",
+				"geo_enabled": true,
+				"verified": false,
+				"statuses_count": 7392,
+				"lang": "en",
+				"contributors_enabled": false,
+				"is_translator": false,
+				"is_translation_enabled": false,
+				"profile_background_color": "352726",
+				"profile_background_image_url": "http://abs.twimg.com/images/themes/theme5/bg.gif",
+				"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme5/bg.gif",
+				"profile_background_tile": false,
+				"profile_image_url": "http://pbs.twimg.com/profile_images/1230905137/Sewface_normal.jpg",
+				"profile_image_url_https": "https://pbs.twimg.com/profile_images/1230905137/Sewface_normal.jpg",
+				"profile_banner_url": "https://pbs.twimg.com/profile_banners/244625496/1356620067",
+				"profile_link_color": "D02B55",
+				"profile_sidebar_border_color": "829D5E",
+				"profile_sidebar_fill_color": "99CC33",
+				"profile_text_color": "3E4415",
+				"profile_use_background_image": true,
+				"has_extended_profile": false,
+				"default_profile": false,
+				"default_profile_image": false,
+				"following": true,
+				"follow_request_sent": false,
+				"notifications": false,
+				"translator_type": "none"
+			},
+			"geo": null,
+			"coordinates": null,
+			"place": null,
+			"contributors": null,
+			"is_quote_status": false,
+			"retweet_count": 2,
+			"favorite_count": 1,
+			"favorited": false,
+			"retweeted": true,
+			"possibly_sensitive": false,
+			"lang": "en"
+		},
+		"is_quote_status": false,
+		"retweet_count": 2,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en"
+	},
+	{
 		"created_at": "Wed Dec 13 02:14:21 +0000 2017",
 		"id": 940766814019162100,
 		"id_str": "940766814019162114",
