12b3154370946841bac3aa09ef575318fb682796
Darren Kuropatwa <dkuropatwa@gmail.com>
2018-02-13 19:16:16 -0600

    Added 1 tweets

---
+++ data/js/tweets/2018_02.js
@@ -0,0 +1,122 @@
+Grailbird.data.tweets_2018_02 = 
+[
+	{
+		"created_at": "Wed Feb 14 00:22:32 +0000 2018",
+		"id": 963569110562033700,
+		"id_str": "963569110562033664",
+		"text": "@EducatePodcast @hechingerreport #mbedchat",
+		"truncated": false,
+		"entities": {
+			"hashtags": [
+				{
+					"text": "mbedchat",
+					"indices": [
+						33,
+						42
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "EducatePodcast",
+					"name": "Educate",
+					"id": 88025330,
+					"id_str": "88025330",
+					"indices": [
+						0,
+						15
+					]
+				},
+				{
+					"screen_name": "hechingerreport",
+					"name": "The Hechinger Report",
+					"id": 121537720,
+					"id_str": "121537720",
+					"indices": [
+						16,
+						32
+					]
+				}
+			],
+			"urls": []
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": 963535491449618400,
+		"in_reply_to_status_id_str": "963535491449618438",
+		"in_reply_to_user_id": 88025330,
+		"in_reply_to_user_id_str": "88025330",
+		"in_reply_to_screen_name": "EducatePodcast",
+		"user": {
+			"id": 5634392,
+			"id_str": "5634392",
+			"name": "Darren Kuropatwa",
+			"screen_name": "dkuropatwa",
+			"location": "Winnipeg, Manitoba, Canada",
+			"description": "Director of Learning at Hanover School Division \nAlways in beta ∞β \n(Tweets represent my own views.)",
+			"url": "http://t.co/FJEo2rqZf5",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "http://t.co/FJEo2rqZf5",
+							"expanded_url": "http://adifference.blogspot.com",
+							"display_url": "adifference.blogspot.com",
+							"indices": [
+								0,
+								22
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 6933,
+			"friends_count": 2608,
+			"listed_count": 464,
+			"created_at": "Sun Apr 29 16:11:17 +0000 2007",
+			"favourites_count": 388,
+			"utc_offset": -21600,
+			"time_zone": "Central Time (US & Canada)",
+			"geo_enabled": false,
+			"verified": true,
+			"statuses_count": 24743,
+			"lang": "en",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "9AE4E8",
+			"profile_background_image_url": "http://pbs.twimg.com/profile_background_images/623182/ADifferenceBG.jpg",
+			"profile_background_image_url_https": "https://pbs.twimg.com/profile_background_images/623182/ADifferenceBG.jpg",
+			"profile_background_tile": true,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1274402174/dkuropatwaGuitar_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1274402174/dkuropatwaGuitar_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/5634392/1399343584",
+			"profile_link_color": "0000FF",
+			"profile_sidebar_border_color": "FFFFFF",
+			"profile_sidebar_fill_color": "67CD67",
+			"profile_text_color": "000000",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"lang": "und"
+	}
+]
