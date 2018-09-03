54aac0d19935beb7b2af51cbe3333eb9d096818c
Darren Kuropatwa <dkuropatwa@gmail.com>
2018-09-03 00:16:15 -0500

    Added 1 tweets

---
+++ data/js/tweets/2018_09.js
@@ -0,0 +1,262 @@
+Grailbird.data.tweets_2018_09 = 
+[
+	{
+		"created_at": "Mon Sep 03 05:06:38 +0000 2018",
+		"id": 1036480566475022300,
+		"id_str": "1036480566475022336",
+		"text": "RT @fryed: 10 Principles of Modern Schools (with 2020 a mere 16 months from today)\n\nhttps://t.co/ikxUonuo12\n\n#nled #nledleaders #bced #abed…",
+		"truncated": false,
+		"entities": {
+			"hashtags": [
+				{
+					"text": "nled",
+					"indices": [
+						109,
+						114
+					]
+				},
+				{
+					"text": "nledleaders",
+					"indices": [
+						115,
+						127
+					]
+				},
+				{
+					"text": "bced",
+					"indices": [
+						128,
+						133
+					]
+				},
+				{
+					"text": "abed",
+					"indices": [
+						134,
+						139
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "fryed",
+					"name": "Donna Miller Fry",
+					"id": 13210942,
+					"id_str": "13210942",
+					"indices": [
+						3,
+						9
+					]
+				}
+			],
+			"urls": [
+				{
+					"url": "https://t.co/ikxUonuo12",
+					"expanded_url": "https://modernlearners.com/10principles/",
+					"display_url": "modernlearners.com/10principles/",
+					"indices": [
+						84,
+						107
+					]
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/#!/download/ipad\" rel=\"nofollow\">Twitter for iPad</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 5634392,
+			"id_str": "5634392",
+			"name": "Darren Kuropatwa",
+			"screen_name": "dkuropatwa",
+			"location": "Winnipeg, Manitoba, Canada",
+			"description": "Educator. Always in beta ∞β.",
+			"url": "https://t.co/FJEo2rIADF",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/FJEo2rIADF",
+							"expanded_url": "http://adifference.blogspot.com",
+							"display_url": "adifference.blogspot.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 6821,
+			"friends_count": 2703,
+			"listed_count": 463,
+			"created_at": "Sun Apr 29 16:11:17 +0000 2007",
+			"favourites_count": 421,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": true,
+			"statuses_count": 24907,
+			"lang": "en",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "9AE4E8",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": true,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1023608435013771265/zRT8t_9Z_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1023608435013771265/zRT8t_9Z_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/5634392/1399343584",
+			"profile_link_color": "0000FF",
+			"profile_sidebar_border_color": "FFFFFF",
+			"profile_sidebar_fill_color": "67CD67",
+			"profile_text_color": "000000",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"retweeted_status": {
+			"created_at": "Sat Sep 01 10:45:56 +0000 2018",
+			"id": 1035841177340260400,
+			"id_str": "1035841177340260354",
+			"text": "10 Principles of Modern Schools (with 2020 a mere 16 months from today)\n\nhttps://t.co/ikxUonuo12\n\n#nled… https://t.co/28QBuows0b",
+			"truncated": true,
+			"entities": {
+				"hashtags": [
+					{
+						"text": "nled",
+						"indices": [
+							98,
+							103
+						]
+					}
+				],
+				"symbols": [],
+				"user_mentions": [],
+				"urls": [
+					{
+						"url": "https://t.co/ikxUonuo12",
+						"expanded_url": "https://modernlearners.com/10principles/",
+						"display_url": "modernlearners.com/10principles/",
+						"indices": [
+							73,
+							96
+						]
+					},
+					{
+						"url": "https://t.co/28QBuows0b",
+						"expanded_url": "https://twitter.com/i/web/status/1035841177340260354",
+						"display_url": "twitter.com/i/web/status/1…",
+						"indices": [
+							105,
+							128
+						]
+					}
+				]
+			},
+			"source": "<a href=\"https://about.twitter.com/products/tweetdeck\" rel=\"nofollow\">TweetDeck</a>",
+			"in_reply_to_status_id": null,
+			"in_reply_to_status_id_str": null,
+			"in_reply_to_user_id": null,
+			"in_reply_to_user_id_str": null,
+			"in_reply_to_screen_name": null,
+			"user": {
+				"id": 13210942,
+				"id_str": "13210942",
+				"name": "Donna Miller Fry",
+				"screen_name": "fryed",
+				"location": "Corner Brook, NL",
+				"description": "Learner, Connected Educator, Network Builder, Assistant Director of Education, Newfoundland & Labrador English School District #edcan #nled #nledleaders",
+				"url": "https://t.co/39EfR6zYNN",
+				"entities": {
+					"url": {
+						"urls": [
+							{
+								"url": "https://t.co/39EfR6zYNN",
+								"expanded_url": "http://blog.donnamillerfry.com",
+								"display_url": "blog.donnamillerfry.com",
+								"indices": [
+									0,
+									23
+								]
+							}
+						]
+					},
+					"description": {
+						"urls": []
+					}
+				},
+				"protected": false,
+				"followers_count": 9825,
+				"friends_count": 10143,
+				"listed_count": 486,
+				"created_at": "Thu Feb 07 16:49:37 +0000 2008",
+				"favourites_count": 29445,
+				"utc_offset": null,
+				"time_zone": null,
+				"geo_enabled": true,
+				"verified": false,
+				"statuses_count": 34143,
+				"lang": "en-gb",
+				"contributors_enabled": false,
+				"is_translator": false,
+				"is_translation_enabled": false,
+				"profile_background_color": "C6E2EE",
+				"profile_background_image_url": "http://abs.twimg.com/images/themes/theme2/bg.gif",
+				"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme2/bg.gif",
+				"profile_background_tile": true,
+				"profile_image_url": "http://pbs.twimg.com/profile_images/989694726356922368/NpDWEkmi_normal.jpg",
+				"profile_image_url_https": "https://pbs.twimg.com/profile_images/989694726356922368/NpDWEkmi_normal.jpg",
+				"profile_banner_url": "https://pbs.twimg.com/profile_banners/13210942/1518322045",
+				"profile_link_color": "C72084",
+				"profile_sidebar_border_color": "C6E2EE",
+				"profile_sidebar_fill_color": "DAECF4",
+				"profile_text_color": "663B12",
+				"profile_use_background_image": true,
+				"has_extended_profile": false,
+				"default_profile": false,
+				"default_profile_image": false,
+				"following": true,
+				"follow_request_sent": false,
+				"notifications": false,
+				"translator_type": "none"
+			},
+			"geo": null,
+			"coordinates": null,
+			"place": null,
+			"contributors": null,
+			"is_quote_status": false,
+			"retweet_count": 9,
+			"favorite_count": 17,
+			"favorited": false,
+			"retweeted": true,
+			"possibly_sensitive": false,
+			"lang": "en"
+		},
+		"is_quote_status": false,
+		"retweet_count": 9,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en"
+	}
+]
