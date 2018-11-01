abfe1442350c157eac1685994a8f7b3d64256f97
Darren Kuropatwa <dkuropatwa@gmail.com>
2018-11-01 16:16:15 -0500

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 24932,
-	"created_at": "Fri Sep 14 11:16:14 +0000 2018",
+	"tweets": 24933,
+	"created_at": "Thu Nov 01 21:16:14 +0000 2018",
 	"lang": "en"
 }
---
+++ data/js/tweets/2018_11.js
@@ -0,0 +1,216 @@
+Grailbird.data.tweets_2018_11 = 
+[
+	{
+		"created_at": "Thu Nov 01 20:59:48 +0000 2018",
+		"id": 1058101324116648000,
+		"id_str": "1058101324116647936",
+		"full_text": "#sp7learns writing #6wordstory for #GRA18 reading about Joseph in #GRArefugee Stident work coming soon. https://t.co/hu3PguXvkD https://t.co/8VnqhvH6Me",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			127
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "sp7learns",
+					"indices": [
+						0,
+						10
+					]
+				},
+				{
+					"text": "6wordstory",
+					"indices": [
+						19,
+						30
+					]
+				},
+				{
+					"text": "GRA18",
+					"indices": [
+						35,
+						41
+					]
+				},
+				{
+					"text": "GRArefugee",
+					"indices": [
+						66,
+						77
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/hu3PguXvkD",
+					"expanded_url": "http://j.mp/2P4GQv9",
+					"display_url": "j.mp/2P4GQv9",
+					"indices": [
+						104,
+						127
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1058101322719940600,
+					"id_str": "1058101322719940610",
+					"indices": [
+						128,
+						151
+					],
+					"media_url": "http://pbs.twimg.com/media/Dq8hjAUX0AIrtxn.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/Dq8hjAUX0AIrtxn.jpg",
+					"url": "https://t.co/8VnqhvH6Me",
+					"display_url": "pic.twitter.com/8VnqhvH6Me",
+					"expanded_url": "https://twitter.com/dkuropatwa/status/1058101324116647936/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 640,
+							"h": 493,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 640,
+							"h": 493,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 640,
+							"h": 493,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1058101322719940600,
+					"id_str": "1058101322719940610",
+					"indices": [
+						128,
+						151
+					],
+					"media_url": "http://pbs.twimg.com/media/Dq8hjAUX0AIrtxn.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/Dq8hjAUX0AIrtxn.jpg",
+					"url": "https://t.co/8VnqhvH6Me",
+					"display_url": "pic.twitter.com/8VnqhvH6Me",
+					"expanded_url": "https://twitter.com/dkuropatwa/status/1058101324116647936/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 640,
+							"h": 493,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 640,
+							"h": 493,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 640,
+							"h": 493,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://ifttt.com\" rel=\"nofollow\">IFTTT</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 5634392,
+			"id_str": "5634392",
+			"name": "Darren Kuropatwa",
+			"screen_name": "dkuropatwa",
+			"location": "Winnipeg, Manitoba, Canada",
+			"description": "Educator. Always in beta ∞β.",
+			"url": "https://t.co/FJEo2rIADF",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/FJEo2rIADF",
+							"expanded_url": "http://adifference.blogspot.com",
+							"display_url": "adifference.blogspot.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 6845,
+			"friends_count": 2701,
+			"listed_count": 461,
+			"created_at": "Sun Apr 29 16:11:17 +0000 2007",
+			"favourites_count": 421,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": true,
+			"statuses_count": 24912,
+			"lang": "en",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "9AE4E8",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": true,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1023608435013771265/zRT8t_9Z_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1023608435013771265/zRT8t_9Z_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/5634392/1399343584",
+			"profile_link_color": "0000FF",
+			"profile_sidebar_border_color": "FFFFFF",
+			"profile_sidebar_fill_color": "67CD67",
+			"profile_text_color": "000000",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "#sp7learns writing #6wordstory for #GRA18 reading about Joseph in #GRArefugee Stident work coming soon. https://t.co/hu3PguXvkD https://t.co/8VnqhvH6Me"
+	}
+]
