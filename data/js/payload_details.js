0d804e63f8d006cca5511a563feb686a2e6abef1
Darren Kuropatwa <dkuropatwa@gmail.com>
2018-12-01 22:26:42 -0600

    Added 2 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 25253,
-	"created_at": "Sun Dec 02 03:16:16 +0000 2018",
+	"tweets": 25255,
+	"created_at": "Sun Dec 02 04:26:41 +0000 2018",
 	"lang": "en"
 }
--- data/js/tweets/2018_12.js
+++ data/js/tweets/2018_12.js
@@ -1,148 +1,176 @@
 Grailbird.data.tweets_2018_12 = 
 [
 	{
-		"created_at": "Fri Jun 08 15:21:08 +0000 2018",
-		"id": 1005107466881962000,
-		"id_str": "1005107466881961989",
-		"full_text": "new post: K12 Online Conference Announcement and Archives (June 2018) https://t.co/R6eEuzeIGP #edtech #k12online https://t.co/ElWHlvwGeL",
+		"created_at": "Sun Dec 02 04:16:51 +0000 2018",
+		"id": 1069082943925223400,
+		"id_str": "1069082943925223424",
+		"full_text": "@sewilkie @edutopia @BalancEdTech @BrianJMull @tsocko @rlfreedm @AmySpicer1 @BillDuffany @JoyKirr @swatanuki1 @sciwilliam1 @OlafCarlson @GarthHolman I have kids do a weekly reflection in a google form: “I used to think ... Now I think ...” about anything from the week. Form feeds a ssheet and filters to tabs for each kid which I can share w parents.",
 		"truncated": false,
 		"display_text_range": [
-			0,
-			112
+			149,
+			351
 		],
 		"entities": {
-			"hashtags": [
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
 				{
-					"text": "edtech",
+					"screen_name": "sewilkie",
+					"name": "sara wilkie",
+					"id": 244625496,
+					"id_str": "244625496",
 					"indices": [
-						94,
-						101
+						0,
+						9
 					]
 				},
 				{
-					"text": "k12online",
+					"screen_name": "edutopia",
+					"name": "edutopia",
+					"id": 35415477,
+					"id_str": "35415477",
 					"indices": [
-						102,
-						112
+						10,
+						19
+					]
+				},
+				{
+					"screen_name": "BalancEdTech",
+					"name": "BalancEdTech",
+					"id": 44669023,
+					"id_str": "44669023",
+					"indices": [
+						20,
+						33
+					]
+				},
+				{
+					"screen_name": "BrianJMull",
+					"name": "Brian Mull",
+					"id": 5667012,
+					"id_str": "5667012",
+					"indices": [
+						34,
+						45
+					]
+				},
+				{
+					"screen_name": "tsocko",
+					"name": "Tracy Sockalosky",
+					"id": 25666964,
+					"id_str": "25666964",
+					"indices": [
+						46,
+						53
+					]
+				},
+				{
+					"screen_name": "rlfreedm",
+					"name": "Rayna Freedman",
+					"id": 2364667867,
+					"id_str": "2364667867",
+					"indices": [
+						54,
+						63
+					]
+				},
+				{
+					"screen_name": "AmySpicer1",
+					"name": "Amy Spicer",
+					"id": 393717723,
+					"id_str": "393717723",
+					"indices": [
+						64,
+						75
+					]
+				},
+				{
+					"screen_name": "BillDuffany",
+					"name": "Bill Duffany",
+					"id": 2411387695,
+					"id_str": "2411387695",
+					"indices": [
+						76,
+						88
+					]
+				},
+				{
+					"screen_name": "JoyKirr",
+					"name": "Joy Kirr",
+					"id": 344350209,
+					"id_str": "344350209",
+					"indices": [
+						89,
+						97
+					]
+				},
+				{
+					"screen_name": "swatanuki1",
+					"name": "Shannon Watanuki",
+					"id": 2730070536,
+					"id_str": "2730070536",
+					"indices": [
+						98,
+						109
+					]
+				},
+				{
+					"screen_name": "sciwilliam1",
+					"name": "Amarilys William",
+					"id": 738360303012843500,
+					"id_str": "738360303012843520",
+					"indices": [
+						110,
+						122
+					]
+				},
+				{
+					"screen_name": "OlafCarlson",
+					"name": "Olaf Carlson",
+					"id": 2157705194,
+					"id_str": "2157705194",
+					"indices": [
+						123,
+						135
+					]
+				},
+				{
+					"screen_name": "GarthHolman",
+					"name": "Garth Holman",
+					"id": 141706548,
+					"id_str": "141706548",
+					"indices": [
+						136,
+						148
 					]
 				}
 			],
-			"symbols": [],
-			"user_mentions": [],
-			"urls": [
-				{
-					"url": "https://t.co/R6eEuzeIGP",
-					"expanded_url": "http://k12onlineconference.org/2018/06/08/k12-online-conference-announcement-and-archives-june-2018/",
-					"display_url": "k12onlineconference.org/2018/06/08/k12…",
-					"indices": [
-						70,
-						93
-					]
-				}
-			],
-			"media": [
-				{
-					"id": 1005105866306015200,
-					"id_str": "1005105866306015232",
-					"indices": [
-						113,
-						136
-					],
-					"media_url": "http://pbs.twimg.com/media/DfLadcrU0AAPHTR.jpg",
-					"media_url_https": "https://pbs.twimg.com/media/DfLadcrU0AAPHTR.jpg",
-					"url": "https://t.co/ElWHlvwGeL",
-					"display_url": "pic.twitter.com/ElWHlvwGeL",
-					"expanded_url": "https://twitter.com/k12online/status/1005107466881961989/photo/1",
-					"type": "photo",
-					"sizes": {
-						"medium": {
-							"w": 1200,
-							"h": 675,
-							"resize": "fit"
-						},
-						"thumb": {
-							"w": 150,
-							"h": 150,
-							"resize": "crop"
-						},
-						"large": {
-							"w": 2048,
-							"h": 1152,
-							"resize": "fit"
-						},
-						"small": {
-							"w": 680,
-							"h": 383,
-							"resize": "fit"
-						}
-					}
-				}
-			]
+			"urls": []
 		},
-		"extended_entities": {
-			"media": [
-				{
-					"id": 1005105866306015200,
-					"id_str": "1005105866306015232",
-					"indices": [
-						113,
-						136
-					],
-					"media_url": "http://pbs.twimg.com/media/DfLadcrU0AAPHTR.jpg",
-					"media_url_https": "https://pbs.twimg.com/media/DfLadcrU0AAPHTR.jpg",
-					"url": "https://t.co/ElWHlvwGeL",
-					"display_url": "pic.twitter.com/ElWHlvwGeL",
-					"expanded_url": "https://twitter.com/k12online/status/1005107466881961989/photo/1",
-					"type": "photo",
-					"sizes": {
-						"medium": {
-							"w": 1200,
-							"h": 675,
-							"resize": "fit"
-						},
-						"thumb": {
-							"w": 150,
-							"h": 150,
-							"resize": "crop"
-						},
-						"large": {
-							"w": 2048,
-							"h": 1152,
-							"resize": "fit"
-						},
-						"small": {
-							"w": 680,
-							"h": 383,
-							"resize": "fit"
-						}
-					}
-				}
-			]
-		},
-		"source": "<a href=\"http://twitter.com\" rel=\"nofollow\">Twitter Web Client</a>",
-		"in_reply_to_status_id": null,
-		"in_reply_to_status_id_str": null,
-		"in_reply_to_user_id": null,
-		"in_reply_to_user_id_str": null,
-		"in_reply_to_screen_name": null,
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": 1064610250383339500,
+		"in_reply_to_status_id_str": "1064610250383339520",
+		"in_reply_to_user_id": 244625496,
+		"in_reply_to_user_id_str": "244625496",
+		"in_reply_to_screen_name": "sewilkie",
 		"user": {
-			"id": 9009432,
-			"id_str": "9009432",
-			"name": "k12online",
-			"screen_name": "k12online",
-			"location": "Third planet from the sun.",
-			"description": "Free worldwide online conference for educators centered on 21st century learning. Archived presentations from 2006-2017.",
-			"url": "http://t.co/MfzVEcowbq",
+			"id": 5634392,
+			"id_str": "5634392",
+			"name": "Darren Kuropatwa",
+			"screen_name": "dkuropatwa",
+			"location": "Winnipeg, Manitoba, Canada",
+			"description": "Educator. Always in beta ∞β.",
+			"url": "https://t.co/FJEo2rIADF",
 			"entities": {
 				"url": {
 					"urls": [
 						{
-							"url": "http://t.co/MfzVEcowbq",
-							"expanded_url": "http://k12onlineconference.org",
-							"display_url": "k12onlineconference.org",
+							"url": "https://t.co/FJEo2rIADF",
+							"expanded_url": "http://adifference.blogspot.com",
+							"display_url": "adifference.blogspot.com",
 							"indices": [
 								0,
-								22
+								23
 							]
 						}
 					]
@@ -152,36 +180,36 @@ Grailbird.data.tweets_2018_12 =
 				}
 			},
 			"protected": false,
-			"followers_count": 6056,
-			"friends_count": 2262,
-			"listed_count": 311,
-			"created_at": "Fri Sep 21 04:27:38 +0000 2007",
-			"favourites_count": 123,
+			"followers_count": 6835,
+			"friends_count": 2702,
+			"listed_count": 459,
+			"created_at": "Sun Apr 29 16:11:17 +0000 2007",
+			"favourites_count": 421,
 			"utc_offset": null,
 			"time_zone": null,
 			"geo_enabled": false,
-			"verified": false,
-			"statuses_count": 1754,
+			"verified": true,
+			"statuses_count": 24916,
 			"lang": "en",
 			"contributors_enabled": false,
 			"is_translator": false,
 			"is_translation_enabled": false,
-			"profile_background_color": "0099B9",
-			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme4/bg.gif",
-			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme4/bg.gif",
-			"profile_background_tile": false,
-			"profile_image_url": "http://pbs.twimg.com/profile_images/1523736683/k12online-logo-twitter-square_normal.jpg",
-			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1523736683/k12online-logo-twitter-square_normal.jpg",
-			"profile_banner_url": "https://pbs.twimg.com/profile_banners/9009432/1528474435",
-			"profile_link_color": "0099B9",
-			"profile_sidebar_border_color": "5ED4DC",
-			"profile_sidebar_fill_color": "95E8EC",
-			"profile_text_color": "3C3940",
+			"profile_background_color": "9AE4E8",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": true,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1023608435013771265/zRT8t_9Z_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1023608435013771265/zRT8t_9Z_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/5634392/1399343584",
+			"profile_link_color": "0000FF",
+			"profile_sidebar_border_color": "FFFFFF",
+			"profile_sidebar_fill_color": "67CD67",
+			"profile_text_color": "000000",
 			"profile_use_background_image": true,
-			"has_extended_profile": false,
+			"has_extended_profile": true,
 			"default_profile": false,
 			"default_profile_image": false,
-			"following": true,
+			"following": false,
 			"follow_request_sent": false,
 			"notifications": false,
 			"translator_type": "none"
@@ -191,13 +219,12 @@ Grailbird.data.tweets_2018_12 =
 		"place": null,
 		"contributors": null,
 		"is_quote_status": false,
-		"retweet_count": 2,
-		"favorite_count": 4,
+		"retweet_count": 0,
+		"favorite_count": 0,
 		"favorited": false,
-		"retweeted": true,
-		"possibly_sensitive": false,
+		"retweeted": false,
 		"lang": "en",
-		"text": "new post: K12 Online Conference Announcement and Archives (June 2018) https://t.co/R6eEuzeIGP #edtech #k12online https://t.co/ElWHlvwGeL"
+		"text": "@sewilkie @edutopia @BalancEdTech @BrianJMull @tsocko @rlfreedm @AmySpicer1 @BillDuffany @JoyKirr @swatanuki1 @sciwilliam1 @OlafCarlson @GarthHolman I have kids do a weekly reflection in a google form: “I used to think ... Now I think ...” about anything from the week. Form feeds a ssheet and filters to tabs for each kid which I can share w parents."
 	},
 	{
 		"created_at": "Fri Jun 08 15:21:08 +0000 2018",
